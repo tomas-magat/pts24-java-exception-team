e23b5c4dcb9e6f6b5439f0f64dfd63ed5df59959
alexdi906 <alex.diko8@gmail.com>
2024-11-14 21:13:16 +0100

    add AllPlayersTakeARewardState class

---
+++ src/main/java/sk/uniba/fmph/dcs/game_phase_controller/AllPlayersTakeARewardState.java
@@ -0,0 +1,68 @@
+package sk.uniba.fmph.dcs.game_phase_controller;
+
+import sk.uniba.fmph.dcs.stone_age.*;
+
+import java.util.Collection;
+
+public class AllPlayersTakeARewardState implements InterfaceGamePhaseState{
+
+    private InterfaceTakeReward takeReward;
+    public AllPlayersTakeARewardState(InterfaceTakeReward takeReward){
+
+        this.takeReward = takeReward;
+
+    }
+    @Override
+    public ActionResult placeFigures(PlayerOrder player, Location location, int figuresCount) {
+
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult makeAction(PlayerOrder player, Location location, Collection<Effect> inputResources, Collection<Effect> outputResources) {
+
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult skipAction(PlayerOrder player, Location location) {
+
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult useTools(PlayerOrder player, int toolIndex) {
+
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult noMoreToolsThisThrow(PlayerOrder player) {
+
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult feedTribe(PlayerOrder player, Collection<Effect> resources) {
+
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult doNotFeedThisTurn(PlayerOrder player) {
+
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult makeAllPlayersTakeARewardChoice(PlayerOrder player, Effect reward) {
+
+        return takeReward.takeReward(player, reward)?ActionResult.ACTION_DONE:ActionResult.FAILURE;
+    }
+
+    @Override
+    public HasAction tryToMakeAutomaticAction(PlayerOrder player) {
+
+        return takeReward.tryMakeAction(player);
+    }
+}
