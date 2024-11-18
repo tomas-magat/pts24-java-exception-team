673bfc5abe8f79b3281774a7a808b6ff215c1aba
matozelenak <zelenak30@uniba.sk>
2024-11-18 18:25:34 +0100

    implement methods in ToolMakerHutFields

--- src/main/java/sk/uniba/fmph/dcs/game_board/ToolMakerHutFields.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/ToolMakerHutFields.java
@@ -1,25 +1,33 @@
 package sk.uniba.fmph.dcs.game_board;
 
+import org.json.JSONObject;
+import sk.uniba.fmph.dcs.stone_age.Effect;
+import sk.uniba.fmph.dcs.stone_age.InterfaceGetState;
 import sk.uniba.fmph.dcs.stone_age.PlayerOrder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
-public class ToolMakerHutFields {
+public class ToolMakerHutFields implements InterfaceGetState {
 
     private List<PlayerOrder> toolMakerFigures;
     private List<PlayerOrder> hutFigures;
     private List<PlayerOrder> fieldsFigures;
-    private int restriction; // no idea
+    private int restriction;
+    private int locationsOccupied;
 
     public ToolMakerHutFields() {
         toolMakerFigures = new ArrayList<>();
         hutFigures = new ArrayList<>();
         fieldsFigures = new ArrayList<>();
+        locationsOccupied = 0;
     }
 
     public boolean placeOnToolMaker(Player player) {
         if (!canPlaceOnToolMaker(player)) return false;
+        locationsOccupied++;
         toolMakerFigures.add(player.getPlayerOrder());
         player.getPlayerBoard().takeFigures(1);
         return true;
@@ -27,10 +35,14 @@ public class ToolMakerHutFields {
 
     public boolean actionToolMaker(Player player) {
         // TODO
-        return false;
+        if (!toolMakerFigures.contains(player.getPlayerOrder())) return false;
+        player.getPlayerBoard().takeFigures(-1);
+        player.getPlayerBoard().giveEffect(new Effect[] {Effect.TOOL});
+        return true;
     }
 
     public boolean canPlaceOnToolMaker(Player player) {
+        if (player.getPlayerOrder().getPlayers() < 4 && locationsOccupied >= 2) return false;
         if (!toolMakerFigures.isEmpty()) return false;
         if (!player.getPlayerBoard().hasFigures(1)) return false;
         return true;
@@ -38,6 +50,7 @@ public class ToolMakerHutFields {
 
     public boolean placeOnHut(Player player) {
         if (!canPlaceOnHut(player)) return false;
+        locationsOccupied++;
         // place 2 figures
         hutFigures.add(player.getPlayerOrder());
         hutFigures.add(player.getPlayerOrder());
@@ -47,10 +60,13 @@ public class ToolMakerHutFields {
 
     public boolean actionHut(Player player) {
         // TODO
+        player.getPlayerBoard().takeFigures(-2);
+        // este treba zvysit totalFigures, kedze hrac ma ziskat figurku navyse
         return false;
     }
 
     public boolean canPlaceOnHut(Player player) {
+        if (player.getPlayerOrder().getPlayers() < 4 && locationsOccupied >= 2) return false;
         if (!hutFigures.isEmpty()) return false;
         if (!player.getPlayerBoard().hasFigures(2)) return false;
         return true;
@@ -58,17 +74,21 @@ public class ToolMakerHutFields {
 
     public boolean placeOnFields(Player player) {
         if (!canPlaceOnFields(player)) return false;
+        locationsOccupied++;
         fieldsFigures.add(player.getPlayerOrder());
         player.getPlayerBoard().takeFigures(1);
         return true;
     }
 
     public boolean actionFields(Player player) {
-        // TODO
-        return false;
+        if (!fieldsFigures.contains(player.getPlayerOrder())) return false;
+        player.getPlayerBoard().takeFigures(-1);
+        player.getPlayerBoard().giveEffect(new Effect[] {Effect.FIELD});
+        return true;
     }
 
     public boolean canPlaceOnFields(Player player) {
+        if (player.getPlayerOrder().getPlayers() < 4 && locationsOccupied >= 2) return false;
         if (!fieldsFigures.isEmpty()) return false;
         if (!player.getPlayerBoard().hasFigures(1)) return false;
         return true;
@@ -78,10 +98,22 @@ public class ToolMakerHutFields {
         toolMakerFigures.clear();
         hutFigures.clear();
         fieldsFigures.clear();
+        locationsOccupied = 0;
     }
 
-    public String getState() {
-        // TODO
-        return null;
+    @Override
+    public String state() {
+        Map<String, String> state = Map.of(
+                "toolMakerFigures", toolMakerFigures.toString(),
+                "hutFigures", hutFigures.toString(),
+                "fieldFigures", fieldsFigures.toString(),
+                "locationsOccupied", locationsOccupied+"");
+
+        return new JSONObject(state).toString();
+    }
+
+    // for testing
+    public int getLocationsOccupied() {
+        return locationsOccupied;
     }
 }
