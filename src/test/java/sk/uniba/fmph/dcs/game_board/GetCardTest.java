6bcebeac967098a81164a15116558cd5621be33b
[nightmarra] <139653260+nightmarra@users.noreply.github.com>
2024-11-28 22:32:13 +0100

    vsetko fixed snad

--- src/test/java/sk/uniba/fmph/dcs/game_board/GetCardTest.java
+++ src/test/java/sk/uniba/fmph/dcs/game_board/GetCardTest.java
@@ -1,2 +1,5 @@
-package sk.uniba.fmph.dcs.game_board;public class GetCardTest {
+package sk.uniba.fmph.dcs.game_board;
+
+public class GetCardTest {
+    // TODO – sociable test, unit doesn't make sense
 }
