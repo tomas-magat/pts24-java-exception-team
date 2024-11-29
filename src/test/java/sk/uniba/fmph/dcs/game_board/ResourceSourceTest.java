ee62eff27eec6b8fdbcbe964887d5b00c1496536
matozelenak <zelenak30@uniba.sk>
2024-11-29 19:57:22 +0100

    update ResourceSourceTest

--- src/test/java/sk/uniba/fmph/dcs/game_board/ResourceSourceTest.java
+++ src/test/java/sk/uniba/fmph/dcs/game_board/ResourceSourceTest.java
@@ -2,12 +2,12 @@ package sk.uniba.fmph.dcs.game_board;
 
 import org.junit.Before;
 import org.junit.Test;
-import sk.uniba.fmph.dcs.stone_age.ActionResult;
-import sk.uniba.fmph.dcs.stone_age.Effect;
-import sk.uniba.fmph.dcs.stone_age.PlayerOrder;
+import sk.uniba.fmph.dcs.stone_age.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.OptionalInt;
 
 import static org.junit.Assert.*;
 
@@ -21,10 +21,10 @@ public class ResourceSourceTest {
 
     @Before
     public void setup() {
-        player1 = new Player(new PlayerOrder(0, 4), null);
-        player2 = new Player(new PlayerOrder(1, 4), null);
-        player3 = new Player(new PlayerOrder(2, 4), null);
-        player4 = new Player(new PlayerOrder(3, 4), null);
+        player1 = new Player(new PlayerOrder(0, 4), new PlayerBoardMock());
+        player2 = new Player(new PlayerOrder(1, 4), new PlayerBoardMock());
+        player3 = new Player(new PlayerOrder(2, 4), new PlayerBoardMock());
+        player4 = new Player(new PlayerOrder(3, 4), new PlayerBoardMock());
         currentThrow = new CurrentThrowMock();
     }
 
@@ -58,6 +58,9 @@ public class ResourceSourceTest {
 
         assertTrue(resourceSource.placeFigures(player1, 1));
         assertTrue(resourceSource.placeFigures(player2, 2));
+
+        assertFalse(resourceSource.placeFigures(player1, 1));
+        assertFalse(resourceSource.placeFigures(player2, 2));
     }
     @Test
     public void testPlaceFigures2Players() {
@@ -75,11 +78,55 @@ public class ResourceSourceTest {
 
     @Test
     public void testMakeActionWrongPlayer() {
-        ResourceSource resourceSource = new ResourceSource(null, "Forest", Effect.WOOD, 7, 4);
+        ResourceSource resourceSource = new ResourceSource(currentThrow, "Forest", Effect.WOOD, 7, 4);
         Collection<Effect> outputResources = new ArrayList<>();
 
         currentThrow.expectedThrowResult = 1;
         assertEquals(resourceSource.makeAction(player1, null, outputResources), ActionResult.FAILURE);
+        assertEquals(resourceSource.makeAction(player2, null, outputResources), ActionResult.FAILURE);
+    }
+
+    @Test
+    public void testMakeActionCorrectPlayer() {
+        ResourceSource resourceSource = new ResourceSource(currentThrow, "Forest", Effect.WOOD, 7, 4);
+        Collection<Effect> outputResources = new ArrayList<>();
+
+        currentThrow.expectedThrowResult = 4;
+        Effect[] expectedList = new Effect[] { Effect.WOOD, Effect.WOOD, Effect.WOOD, Effect.WOOD };
+        PlayerBoardMock playerBoardMock1 = (PlayerBoardMock) player1.getPlayerBoard();
+
+        assertTrue(resourceSource.placeFigures(player1, 3));
+        assertEquals(resourceSource.makeAction(player1, null, outputResources), ActionResult.ACTION_DONE);
+        assertEquals(expectedList, playerBoardMock1.givenEffects);
+
+        assertEquals(resourceSource.makeAction(player2, null, outputResources), ActionResult.FAILURE);
+    }
+
+    @Test
+    public void testMakeActionCorrectPlayer2() {
+        ResourceSource resourceSource = new ResourceSource(currentThrow, "Quarry", Effect.STONE, 7, 4);
+        Collection<Effect> outputResources = new ArrayList<>();
+
+        currentThrow.expectedThrowResult = 2;
+        Effect[] expectedList = new Effect[] { Effect.STONE, Effect.STONE };
+        PlayerBoardMock playerBoardMock1 = (PlayerBoardMock) player1.getPlayerBoard();
+
+        assertTrue(resourceSource.placeFigures(player1, 3));
+        assertEquals(resourceSource.makeAction(player1, null, outputResources), ActionResult.ACTION_DONE);
+        assertEquals(expectedList, playerBoardMock1.givenEffects);
+
+        outputResources.clear();
+        currentThrow.expectedThrowResult = 3;
+        expectedList = new Effect[] { Effect.STONE, Effect.STONE, Effect.STONE };
+        PlayerBoardMock playerBoardMock2 = (PlayerBoardMock) player2.getPlayerBoard();
+
+        assertTrue(resourceSource.placeFigures(player2, 3));
+        assertEquals(resourceSource.makeAction(player2, null, outputResources), ActionResult.ACTION_DONE);
+        assertEquals(expectedList, playerBoardMock2.givenEffects);
+
+
+
+        assertEquals(resourceSource.makeAction(player3, null, outputResources), ActionResult.FAILURE);
     }
 
 
@@ -100,6 +147,53 @@ public class ResourceSourceTest {
         @Override
         public String state() {
             return null; // don't need
+        }
+    }
+
+    private static class PlayerBoardMock implements InterfacePlayerBoardGameBoard {
+        public boolean expectedHasFigures;
+        public int takeFiguresAmount = 0;
+        public Effect[] givenEffects;
+
+        @Override
+        public void giveEffect(Effect[] stuff) {
+            this.givenEffects = stuff;
+        }
+
+        @Override
+        public void giveEndOfGameEffect(EndOfGameEffect[] stuff) {
+
+        }
+
+        @Override
+        public boolean takeResources(Effect[] stuff) {
+            return false;
+        }
+
+        @Override
+        public boolean takeFigures(int count) {
+            takeFiguresAmount = count;
+            return false;
+        }
+
+        @Override
+        public boolean hasFigures(int count) {
+            return expectedHasFigures;
+        }
+
+        @Override
+        public boolean hasSufficientTools(int goal) {
+            return false;
+        }
+
+        @Override
+        public OptionalInt useTool(int idx) {
+            return null;
+        }
+
+        @Override
+        public boolean addNewFigure() {
+            return false;
         }
     }
 }
