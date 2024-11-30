dbd01bdd42c292218d59b5b121a7352a5838d5ef
samosato <matejlorinc17@gmail.com>
2024-11-30 14:32:23 +0100

    Finished whole, unsure if good

--- src/main/java/sk/uniba/fmph/dcs/game_board/GetSomethingThrow.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/GetSomethingThrow.java
@@ -6,28 +6,50 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class GetSomethingThrow implements EvaluateCivilisationCardImmediateEffect {
-    private ArrayList<Effect> resource;
-    private CurrentThrow currentThrow;
+    private Effect[] resources;
 
-    public GetSomethingThrow(final Collection<Effect> resources, CurrentThrow currentThrow) {
-        this.resource = new ArrayList<Effect>();
-        this.currentThrow = currentThrow;
+    public GetSomethingThrow(Effect[] resources) {
         for (Effect resource : resources) {
             if (!resource.isResource()) {
                 throw new IllegalArgumentException("Resources must be resource");
             }
-            this.resource.add(resource);
+        this.resources = resources;
         }
     }
 
     @Override
     public boolean performEffect(Player player, Effect choice) {
-        if (resource != null && !resource.isEmpty()) {
-            player.getPlayerBoard().giveEffect(new Effect[]{choice});
-            System.out.println("Effect " + choice + " applied to player " + player.getPlayerOrder());
-            return true;
-        }
+        CurrentThrow currentThrow = new CurrentThrow();
 
+        currentThrow.initiate(player, choice, 2);
+
+        int throwResult = currentThrow.getThrowResult();
+
+        int resourcesGained = throwResult / choice.points();
+
+        Effect[] gainedResources = createEffectArray(choice, resourcesGained);
+
+        player.getPlayerBoard().giveEffect(gainedResources);
+
+        System.out.println("Effect " + choice + " applied to player " + player.getPlayerOrder());
+
+        return false;
+    }
+
+    private Effect[] createEffectArray(Effect effect, int count) {
+        Effect[] effects = new Effect[count];
+        for (int i = 0; i < count; i++) {
+            effects[i] = effect;
+        }
+        return effects;
+    }
+
+    private boolean isAllowedResource(Effect choice) {
+        for(Effect resource : resources) {
+            if (resource == choice) {
+                return true;
+            }
+        }
         return false;
     }
 }
