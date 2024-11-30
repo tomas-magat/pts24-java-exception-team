8283e4ebc5e67b0fbfa46f6c663ac29079d63412
samosato <matejlorinc17@gmail.com>
2024-11-30 15:07:50 +0100

    Final GetChoice, not sure if good

--- src/main/java/sk/uniba/fmph/dcs/game_board/GetChoice.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/GetChoice.java
@@ -4,16 +4,31 @@ import sk.uniba.fmph.dcs.stone_age.Effect;
 
 public class GetChoice implements EvaluateCivilisationCardImmediateEffect {
     private boolean isUsed;
+    private int numberOfResources;
 
-    public GetChoice() {
+    public GetChoice(int numberOfResources) {
         this.isUsed = false;
+        this.numberOfResources = numberOfResources;
     }
-
 
     @Override
     public boolean performEffect(Player player, Effect choice) {
+        if (this.isUsed){
+            return false;
+        }
 
-        return false;
+        if (choice == null || !choice.isResource()) {
+            return false;
+        }
+
+        player.getPlayerBoard().giveEffect(new Effect[] {choice});
+        numberOfResources++;
+
+        if (numberOfResources == 2) {
+            this.isUsed = true;
+        }
+
+        return true;
     }
 
     public boolean isUsed() {
